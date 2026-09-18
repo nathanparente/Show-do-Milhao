d1a3891b9424433389b1085a9b058093844869b5
Nathan Parente <nathan.parente22@gmail.com>
2026-09-18 17:44:34 -0300

    fix: adjustments to new structure

--- src/components/QuestionsList/QuestionsList.vue
+++ src/components/QuestionsList/QuestionsList.vue
@@ -192,12 +192,12 @@ export default {
     handleAnswers(index) {
       this.choice = index;
       if (this.choices[index].isTrue) {
-        this.color = "#57e71d";
+        this.color = "#57e71d"; // Verde
         setTimeout(() => {
           this.rightQuestion();
         }, 1000);
       } else {
-        this.color = "#f60808";
+        this.color = "#f60808"; // Vermelho
         this.wrongQuestion();
       }
     },
@@ -251,6 +251,7 @@ export default {
         callHelp: "",
       });
     },
+
     ...mapMutations(["updateChartData", "updateCallHelp"]),
   },
 };
