32c6a552ffd8507046ee77d91da47ca75e68befc
Nathan Parente <nathan.parente22@gmail.com>
2026-09-21 16:10:44 -0300

    fix: giveUpDialog with arregou gif meme

--- src/components/GiveUpDialog/GiveUpDialog.vue
+++ src/components/GiveUpDialog/GiveUpDialog.vue
@@ -1,8 +1,9 @@
 <template>
   <v-dialog :value="dialog" persistent max-width="400">
     <v-card class="text-center pa-4">
-      <v-card-title class="justify-center"> Desistiu </v-card-title>
-      <v-card-actions class="justify-center">
+      <v-img :src="giveUpGif" contain max-height="250" class="mx-auto" />
+
+      <v-card-actions class="justify-center mt-4">
         <v-btn color="primary" @click="$emit('continue')"> OK </v-btn>
       </v-card-actions>
     </v-card>
@@ -17,6 +18,11 @@ export default {
       type: Boolean,
       default: false,
     },
+  },
+  data() {
+    return {
+      giveUpGif: require("@/assets/images/bg/arregou-silvio.gif"),
+    };
   },
 };
 </script>
